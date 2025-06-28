de2b9d536547480e8d140eb45bad2001a6dd54d5
nyukkiw <valentinobanyu0@gmail.com>
2025-06-28 20:28:07 +0700

    my third lesson (conditional rendering and return value to parent with props)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Home from "./pages/index"
 function App() {
+  const value = false;
+
   return (
-    <>
+    <div className="App">
+     {value ? 'benar' : 'salah'}
       <Home/>
-    </>
+      
+    </div>
   )
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,25 @@
-import posts from "../posts.json";
+import { useState } from "react";
+import postsData from "../posts.json";
 import Articel from "../components/Article";
+import Search from "../components/Search";
 
 function Homepage(){
+    const [posts, setPosts]=useState(postsData)
+    const [totalPosts, setTotalPosts]=useState(0);
+
+    const onSearchChange=(value)=>{
+        const filteredPosts = postsData.filter(item=>item.title.includes(value));
+        setPosts(filteredPosts);
+        setTotalPosts(filteredPosts.length);
+    };
+
     return(
         <>
             <h1>Simple blog</h1>
-            {posts.map((blog)=> (
-                    <Articel title={blog.title} tags={blog.tags} date={blog.date}/>
+            <Search onSearchChange={onSearchChange} totalPosts={totalPosts}/>
+
+            {posts.map((props, index)=> (
+                    <Articel {...props} key={index}/>
             ))}
         </>
     );
